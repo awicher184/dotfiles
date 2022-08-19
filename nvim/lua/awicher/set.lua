fd85d69cf2408316b4db11ca2ee7f6af8318acdf
Antoninos Wicher <antoninos_wicher@gmx.de>
2022-08-19 22:46:59 +0200

    change from space to tabs

--- nvim/lua/awicher/set.lua
+++ nvim/lua/awicher/set.lua
@@ -15,9 +15,8 @@ vim.opt.relativenumber = true
 -- tab & indent 
 vim.opt.tabstop = 4 
 vim.opt.shiftwidth = 4 
-vim.opt.softtabstop = 4 
-vim.opt.expandtab = true 
 vim.opt.smartindent = true
+vim.opt.expandtab = false
 
 -- search
 vim.opt.hlsearch = false 
